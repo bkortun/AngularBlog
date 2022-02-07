f4b84e8325f31266c334999e24106c1087892f64
Bkortun <bariskorun@gmail.com>
2022-02-07 17:45:30 +0300

    07.01.2022

--- src/app/admin-pages/article/article-add/article-add.component.ts
+++ src/app/admin-pages/article/article-add/article-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-article-add',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleAddComponent implements OnInit {
 
-  constructor() { }
+  fileData:File=null;
+  picture:string=null;
+  defaultArticle: string = 'assets/article_empty.png';
+  constructor(private articleService:ArticleService) { }
 
   ngOnInit(): void {
   }
 
+  upload(files:any) {
+    this.fileData = files.target.files[0];
+
+    let formData = new FormData();
+
+    formData.append("picture", this.fileData);
+
+    this.articleService.saveArticlePicture(formData).subscribe(result => {
+      console.log(result.path);
+      this.picture=result.path;
+
+    });
+  }
 }
--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -14,38 +14,97 @@ export class ArticleService {
   private apiUrl: string = 'https://localhost:44351/api/Articles';
   getArticles(page: number, pageSize: Number) {
     let url: string = this.apiUrl + '/' + page + '/' + pageSize;
-    return this.httpClient.get<ArticlePg>(url).pipe(tap(x=>{this.loading=false}));
+    return this.httpClient.get<ArticlePg>(url).pipe(
+      tap((x) => {
+        this.loading = false;
+      })
+    );
   }
-  getArticle(id:number){
+  getArticle(id: number) {
     let url: string = this.apiUrl + '/' + id;
-    return this.httpClient.get<Article>(url).pipe(tap(x=>{this.loading=false}));
+    return this.httpClient.get<Article>(url).pipe(
+      tap((x) => {
+        this.loading = false;
+      })
+    );
   }
-  getSearchArticles(searchText:string,page:number,pageSize:number){
-    let url: string = this.apiUrl + '/'+"SearchArticles"+ '/' + searchText + '/' + page + '/' + pageSize;
-    return this.httpClient.get<ArticlePg>(url).pipe(tap(x=>{this.loading=false}));
+  getSearchArticles(searchText: string, page: number, pageSize: number) {
+    let url: string =
+      this.apiUrl +
+      '/' +
+      'SearchArticles' +
+      '/' +
+      searchText +
+      '/' +
+      page +
+      '/' +
+      pageSize;
+    return this.httpClient.get<ArticlePg>(url).pipe(
+      tap((x) => {
+        this.loading = false;
+      })
+    );
   }
-  getArticleWithCategory(id:number,page: number, pageSize: Number){
-    let url: string = this.apiUrl + '/'+"GetArticleWithCategory"+ '/' + id + '/' + page + '/' + pageSize;
-    return this.httpClient.get<ArticlePg>(url).pipe(tap(x=>{this.loading=false}));
+  getArticleWithCategory(id: number, page: number, pageSize: Number) {
+    let url: string =
+      this.apiUrl +
+      '/' +
+      'GetArticleWithCategory' +
+      '/' +
+      id +
+      '/' +
+      page +
+      '/' +
+      pageSize;
+    return this.httpClient.get<ArticlePg>(url).pipe(
+      tap((x) => {
+        this.loading = false;
+      })
+    );
   }
-  getArticlesByMostView(){
-    let url:string =this.apiUrl+"/"+"GetArticlesByMostView";
+  getArticlesByMostView() {
+    let url: string = this.apiUrl + '/' + 'GetArticlesByMostView';
     return this.httpClient.get<Article[]>(url);
   }
-  getArticlesArchive(){
-    let url:string =this.apiUrl+"/"+"GetArticlesArchive";
+  getArticlesArchive() {
+    let url: string = this.apiUrl + '/' + 'GetArticlesArchive';
     return this.httpClient.get<Archive[]>(url);
   }
-  getArticleArchiveList(year:number,month:number,page:number,pageSize:number){
-    let url:string =this.apiUrl+'/'+"GetArticleArchiveList"+ '/' + year + '/'+ month + '/' + page + '/' + pageSize;
-    return this.httpClient.get<ArticlePg>(url).pipe(tap(x=>{ this.loading=false}));
+  getArticleArchiveList(
+    year: number,
+    month: number,
+    page: number,
+    pageSize: number
+  ) {
+    let url: string =
+      this.apiUrl +
+      '/' +
+      'GetArticleArchiveList' +
+      '/' +
+      year +
+      '/' +
+      month +
+      '/' +
+      page +
+      '/' +
+      pageSize;
+    return this.httpClient.get<ArticlePg>(url).pipe(
+      tap((x) => {
+        this.loading = false;
+      })
+    );
   }
-  articleViewCountUp(id:number){
-    let url:string =this.apiUrl+'/'+"ArticleViewCountUp"+ '/' + id;
+  articleViewCountUp(id: number) {
+    let url: string = this.apiUrl + '/' + 'ArticleViewCountUp' + '/' + id;
     return this.httpClient.get(url);
   }
 
-  getArticlesWithoutPg(){
+  getArticlesWithoutPg() {
     return this.httpClient.get<Article[]>(this.apiUrl);
+  }
+
+  saveArticlePicture(image: any) {
+    let url: string = this.apiUrl + '/' + 'SaveArticlePicture';
+    return this.httpClient.post<any>(url,image);
   }
 }
